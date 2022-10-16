4d487a4389ee2f7a7998b12773591ad5740acfc6
“guanfl <13453001@qq.com>
2022-10-16 16:30:44 +0800

    0.3

---
+++ docs/.vitepress/conf/socialLink.js
@@ -0,0 +1,13 @@
+export default  [
+      //内置的icon
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'twitter', link: '...' },
+      // You can also add custom icons by passing SVG as string:
+      {
+        //  icon可以是svg代码
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+        },
+        link: '...'
+      }
+    ]
